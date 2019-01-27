e55f99c3294e209ece14b3dd084c09c1dfd9eabc
liuchunhua <thumb0422@163.com>
2019-01-27 20:49:43 +0800

    update

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -2,6 +2,7 @@
  * 首页 : 全屏、一张背景图、 下半部分多个可滑动的按钮、左下角按钮（投诉）、右下角按钮（新增）
 **/
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -13,16 +14,27 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.yellow,
+//      body: Center(
+//        child: Image.asset('images/home.png',
+//            fit: BoxFit.fill,
+//            width: window.physicalSize.width,
+//            height: window.physicalSize.height),
+//      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+        child: Container(
+          decoration: BoxDecoration(
+              image : DecorationImage(
+                  image:AssetImage('images/home.png'),
+                centerSlice: Rect.fromLTRB(0, 0, 0, 0),//TODO:
+              )
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: null,
         child: Icon(Icons.add),
-      ), 
+      ),
     );
   }
 }
