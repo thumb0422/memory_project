f18649f20b3a99cb6925cd47a5c29138a5dee91d
liuchunhua1 <chunhua1@eastmoney.com>
2019-01-30 16:57:31 +0800

    新增 下拉框、新增页面 修改

--- lib/page/addPage.dart
+++ lib/page/addPage.dart
@@ -2,21 +2,106 @@
  * 新增 页面
 **/
 import 'package:flutter/material.dart';
+import '../view/myDropDownButton.dart';
 
 class AddPage extends StatelessWidget {
+  var value;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('新增记忆仓库'),
       ),
-      body: Center(
+      body: Container(
+        padding: const EdgeInsets.only(top: 42.0),
+        alignment: Alignment.centerLeft,
         child: Column(
           children: <Widget>[
-
+            Row(
+              children: <Widget>[
+                Container(
+                  width: 70.0,
+                  height: 50.0,
+                  margin: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    '类型：',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 17.0,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 5.0, right: 15.0),
+                  height: 48.0,
+                  child: MyDropdownButton(
+                      //TODO:
+                      ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        labelText: "网址",
+                        hintText: "请输入网址",
+                        prefixIcon: Icon(Icons.person)),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        labelText: "账号",
+                        hintText: "请输入账号",
+                        prefixIcon: Icon(Icons.account_box)),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        labelText: "密码",
+                        hintText: "请输入密码",
+                        prefixIcon: Icon(Icons.security)),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Flexible(
+                  child: TextField(
+                    autofocus: true,
+                    decoration: InputDecoration(
+                        labelText: "描述",
+                        hintText: "请输入描述信息",
+                        prefixIcon: Icon(Icons.person)),
+                  ),
+                )
+              ],
+            ),
+            MaterialButton(
+              onPressed: onSaveClick(),
+              child: Text('保       存'),
+            )
           ],
         ),
       ),
     );
   }
+
+  onSaveClick() {}
 }
---
+++ lib/view/myDropDownButton.dart
@@ -0,0 +1,95 @@
+/**
+ * 下拉菜单样式
+ */
+import 'package:flutter/material.dart';
+
+class MyDropdownButton extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyDropdownButton();
+  }
+}
+
+class _MyDropdownButton extends State<MyDropdownButton> {
+  List<DropdownMenuItem> getListData() {
+    List<DropdownMenuItem> items = List();
+    DropdownMenuItem dropdownMenuItem1 = DropdownMenuItem(
+      child: Text('0001'),
+      value: '理财',
+    );
+    items.add(dropdownMenuItem1);
+
+    DropdownMenuItem dropdownMenuItem2 = DropdownMenuItem(
+      child: Text('0002'),
+      value: '社交',
+    );
+    items.add(dropdownMenuItem2);
+
+    DropdownMenuItem dropdownMenuItem3 = DropdownMenuItem(
+      child: Text('0003'),
+      value: '支付',
+    );
+    items.add(dropdownMenuItem3);
+
+    DropdownMenuItem dropdownMenuItem4 = DropdownMenuItem(
+      child: Text('0004'),
+      value: '工作',
+    );
+    items.add(dropdownMenuItem4);
+
+    DropdownMenuItem dropdownMenuItem5 = DropdownMenuItem(
+      child: Text('0005'),
+      value: '邮箱',
+    );
+    items.add(dropdownMenuItem5);
+
+    DropdownMenuItem dropdownMenuItem6 = DropdownMenuItem(
+      child: Text('0006'),
+      value: '购物',
+    );
+    items.add(dropdownMenuItem6);
+
+    DropdownMenuItem dropdownMenuItem7 = DropdownMenuItem(
+      child: Text('0007'),
+      value: '游戏',
+    );
+    items.add(dropdownMenuItem7);
+
+    DropdownMenuItem dropdownMenuItem8 = DropdownMenuItem(
+      child: Text('0008'),
+      value: '其他',
+    );
+    items.add(dropdownMenuItem8);
+
+    return items;
+  }
+
+  var value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        new Center(
+          child: new DropdownButton(
+            items: getListData(),
+            hint: new Text('选择类型'), //当没有默认值的时候可以设置的提示
+            value: value, //下拉菜单选择完之后显示给用户的值
+            onChanged: (T) {
+              //下拉菜单item点击之后的回调
+              setState(() {
+                value = T;
+              });
+            },
+            elevation: 24, //设置阴影的高度
+            style: new TextStyle(
+              //设置文本框里面文字的样式
+                color: Colors.black),
+//              isDense: false,//减少按钮的高度。默认情况下，此按钮的高度与其菜单项的高度相同。如果isDense为true，则按钮的高度减少约一半。 这个当按钮嵌入添加的容器中时，非常有用
+            iconSize: 20.0, //设置三角标icon的大小
+          ),
+        ),
+      ],
+    );
+  }
+}
