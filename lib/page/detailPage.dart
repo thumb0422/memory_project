e49bcda07415580c74459d35c6bef09d262cdcfd
liuchunhua1 <chunhua1@eastmoney.com>
2019-03-11 16:47:45 +0800

    detailPage

--- lib/page/detailPage.dart
+++ lib/page/detailPage.dart
@@ -4,21 +4,65 @@
 
 import 'package:flutter/material.dart';
 import '../model/model.dart';
+import '../utility/tool.dart';
+import '../utility/global.dart';
 
-class DetailPage extends StatelessWidget{
-
+class DetailPage extends StatelessWidget {
   final Model data;
+  final String title;
+  const DetailPage({Key key, this.data, this.title}) : super(key: key);
 
-  const DetailPage({Key key,this.data}):super(key:key);
+//  final _formKey = GlobalKey<FormState>();
+
+  final bool _isObscure = true;
+  final Color _eyeColor = null;
+  static String _urlStr;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-//        title: Text('${data.name}记忆'),
+        title: Text('${this.title}记忆详情'),
       ),
-      backgroundColor: Color.fromRGBO(243, 255, 212, 1),
-      body: Text(this.data.accountDesc),
+      backgroundColor: pageBgColor,
+      body: FutureBuilder(
+          future: getLocalTypeModels(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Form(
+                  child: ListView(
+                children: <Widget>[
+                  SizedBox(
+                    height: 20,
+                  ),
+//                    _buildTypeView(context),
+                    _buildUrlView(context),
+//                    _buildAccountView(context),
+//                    _buildPwdView(context),
+//                    _buildDescView(context),
+                  SizedBox(
+                    height: 40,
+                  ),
+//                    _buildSaveButton(context),
+                ],
+              ));
+            }
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
+    );
+  }
+
+  TextFormField _buildUrlView(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        prefixIcon: Icon(Icons.http),
+        labelText: '网址',
+        hintText: this.data.accountUrl
+      ),
+
+      onSaved: (String value) => _urlStr = value,
     );
   }
 }
